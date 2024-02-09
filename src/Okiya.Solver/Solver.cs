bc0d7f6f6136992cb026692c39567ddf8ee1a77d
Viktor Ptitselov <qbit86@gmail.com>
2024-02-09 02:47:46 +0200

    Add Solve() method stub

--- src/Okiya.Solver/Solver.cs
+++ src/Okiya.Solver/Solver.cs
@@ -1,5 +1,24 @@
-﻿namespace Okiya;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Okiya;
+
+// https://en.wikipedia.org/wiki/Negamax
 
 public sealed class Solver
 {
+    private readonly int[] _board;
+
+    public Solver(int[] board)
+    {
+        ArgumentNullException.ThrowIfNull(board);
+        _board = board;
+    }
+
+    public IReadOnlyList<int> Board => _board;
+
+    public double Solve(Stack<KeyValuePair<int, double>> moveEvaluationStack) => Solve(new(), moveEvaluationStack);
+
+    private double Solve(Node rootNode, Stack<KeyValuePair<int, double>> moveEvaluationStack) =>
+        throw new NotImplementedException();
 }
