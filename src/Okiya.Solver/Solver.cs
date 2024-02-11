5263371d88ab26130743901c20c01dbc527a8839
Viktor Ptitselov <qbit86@gmail.com>
2024-02-11 14:25:19 +0200

    Add Solver.IsTerminalNode()

--- src/Okiya.Solver/Solver.cs
+++ src/Okiya.Solver/Solver.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
 
 namespace Okiya;
 
@@ -8,6 +10,7 @@ namespace Okiya;
 
 public sealed class Solver
 {
+    private static readonly uint[] s_winPatterns = CreateWinPatters();
     private readonly int[] _board;
 
     public Solver(int[] board)
@@ -36,5 +39,44 @@ public sealed class Solver
         throw new NotImplementedException();
     }
 
-    private bool IsTerminalNode(Node node, out double evaluation) => throw new NotImplementedException();
+    private static bool IsTerminalNode(Node node, out double evaluation)
+    {
+        (int sideToMove, int playerTokens) = node.GetSideToMoveAndPlayerTokens();
+        if (!s_winPatterns.Any(p => BitOperations.PopCount(unchecked(p & (uint)playerTokens)) is 4))
+        {
+            evaluation = 0;
+            return false;
+        }
+
+        evaluation = sideToMove is 0 ? double.MaxValue : double.MinValue;
+        return true;
+    }
+
+    private static uint[] CreateWinPatters() =>
+    [
+        0b11110000_00000000u,
+        0b00001111_00000000u,
+        0b00000000_11110000u,
+        0b00000000_00001111u,
+
+        0b10001000_10001000u,
+        0b01000100_01000100u,
+        0b00100010_00100010u,
+        0b00010001_00010001u,
+
+        0b10000100_00100001u,
+        0b00010010_01001000u,
+
+        0b00000000_00110011u,
+        0b00000000_01100110u,
+        0b00000000_11001100u,
+
+        0b00000011_00110000u,
+        0b00000110_01100000u,
+        0b00001100_11000000u,
+
+        0b00110011_00000000u,
+        0b01100110_00000000u,
+        0b11001100_00000000u
+    ];
 }
