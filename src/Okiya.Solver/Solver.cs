c98b7537e810dfb972c0dbf957431500d3be4065
Viktor Ptitselov <qbit86@gmail.com>
2024-02-11 14:42:24 +0200

    Refactor Solver.IsTerminalNode()

--- src/Okiya.Solver/Solver.cs
+++ src/Okiya.Solver/Solver.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Numerics;
 
 namespace Okiya;
 
@@ -10,7 +9,7 @@ namespace Okiya;
 
 public sealed class Solver
 {
-    private static readonly uint[] s_winPatterns = CreateWinPatters();
+    private static readonly int[] s_winPatterns = CreateWinPatters();
     private readonly int[] _board;
 
     public Solver(int[] board)
@@ -39,44 +38,52 @@ public sealed class Solver
         throw new NotImplementedException();
     }
 
+    private int PopulatePossibleMoves(Node node, Span<int> moves) => throw new NotImplementedException();
+
     private static bool IsTerminalNode(Node node, out double evaluation)
     {
         (int sideToMove, int playerTokens) = node.GetSideToMoveAndPlayerTokens();
-        if (!s_winPatterns.Any(p => BitOperations.PopCount(unchecked(p & (uint)playerTokens)) is 4))
+        if (s_winPatterns.Any(p => (p & playerTokens) == p))
         {
-            evaluation = 0;
-            return false;
+            evaluation = sideToMove is 0 ? double.PositiveInfinity : double.NegativeInfinity;
+            return true;
         }
 
-        evaluation = sideToMove is 0 ? double.MaxValue : double.MinValue;
-        return true;
+        if (node.IsFull())
+        {
+            evaluation = 0;
+            return true;
+        }
+
+        evaluation = double.NaN;
+        return false;
     }
 
-    private static uint[] CreateWinPatters() =>
+    private static int[] CreateWinPatters() =>
     [
-        0b11110000_00000000u,
-        0b00001111_00000000u,
-        0b00000000_11110000u,
-        0b00000000_00001111u,
+        0b11110000_00000000,
+        0b00001111_00000000,
+        0b00000000_11110000,
+        0b00000000_00001111,
 
-        0b10001000_10001000u,
-        0b01000100_01000100u,
-        0b00100010_00100010u,
-        0b00010001_00010001u,
+        0b10001000_10001000,
+        0b01000100_01000100,
+        0b00100010_00100010,
+        0b00010001_00010001,
 
-        0b10000100_00100001u,
-        0b00010010_01001000u,
+        0b10000100_00100001,
+        0b00010010_01001000,
 
-        0b00000000_00110011u,
-        0b00000000_01100110u,
-        0b00000000_11001100u,
+        0b00000000_00110011,
+        0b00000000_01100110,
+        0b00000000_11001100,
 
-        0b00000011_00110000u,
-        0b00000110_01100000u,
-        0b00001100_11000000u,
+        0b00000011_00110000,
+        0b00000110_01100000,
+        0b00001100_11000000,
 
-        0b00110011_00000000u,
-        0b01100110_00000000u,
-        0b11001100_00000000u
+        0b00110011_00000000,
+        0b01100110_00000000,
+        0b11001100_00000000
     ];
 }
