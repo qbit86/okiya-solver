ad6971fc3766b594432f46b8be7ab567dbf490d2
Viktor Ptitselov <qbit86@gmail.com>
2024-02-11 11:39:47 +0200

    Refactor Node

--- src/Okiya.Solver/Node.cs
+++ src/Okiya.Solver/Node.cs
@@ -1,57 +1,77 @@
 using System.Globalization;
 using System.Text;
+#if DEBUG
+using System.Diagnostics;
+#endif
 
 namespace Okiya;
 
 internal readonly record struct Node
 {
-    private const uint CardMask = ~(1u << 31);
+    private const int PlayerTokensBitCount = 16;
+
+    private const int PlayerTokensMask = 0xFFFF;
+
+    private const int CardBitCount = 31;
+
+    private const int CardMask = 0x7fffffff;
 
     private readonly uint _cardAndSideToMove;
-    private readonly uint _playersTokenBits;
+    private readonly uint _playersTokens;
 
-    private Node(uint playersTokenBits, uint cardAndSideToMove)
+#if DEBUG
+    static Node()
     {
-        _playersTokenBits = playersTokenBits;
+        Debug.Assert(PlayerTokensBitCount is Constants.CardCount);
+        Debug.Assert(PlayerTokensBitCount > 0);
+        Debug.Assert(1 << PlayerTokensBitCount is PlayerTokensMask + 1);
+        Debug.Assert(CardBitCount > 0);
+        Debug.Assert(1u << CardBitCount is CardMask + 1u);
+    }
+#endif
+
+    private Node(uint playersTokens, uint cardAndSideToMove)
+    {
+        _playersTokens = playersTokens;
         _cardAndSideToMove = cardAndSideToMove;
     }
 
     private bool PrintMembers(StringBuilder builder)
     {
         CultureInfo p = CultureInfo.InvariantCulture;
-        builder.Append(p, $"Players = {GetMinPlayerTokenBits():b16}|{GetMaxPlayerTokenBits():b16}");
+        builder.Append(p, $"Players = {GetMinPlayerTokens():b16}|{GetMaxPlayerTokens():b16}");
         int card = GetCardOrDefault();
         builder.Append(p, $", Card = {card} ({Int32CardConcept.Instance.ToString(card)})");
         return true;
     }
 
-    internal int GetMaxPlayerTokenBits() => unchecked((int)(_playersTokenBits & 0xFFFF));
+    internal int GetMaxPlayerTokens() => unchecked((int)(_playersTokens & PlayerTokensMask));
 
-    internal int GetMinPlayerTokenBits() => unchecked((int)(_playersTokenBits >> Constants.CardCount));
+    internal int GetMinPlayerTokens() => unchecked((int)(_playersTokens >> PlayerTokensBitCount));
 
     internal int GetCardOrDefault() => unchecked((int)(_cardAndSideToMove & CardMask));
 
     internal bool TryGetCard(out int card)
     {
         card = GetCardOrDefault();
-        return _playersTokenBits is not 0;
+        return _playersTokens is not 0;
     }
 
-    internal int SideToMove() => unchecked((int)(_cardAndSideToMove >> 31));
+    internal int SideToMove() => unchecked((int)(_cardAndSideToMove >> CardBitCount));
 
     internal Node AddMaxPlayerToken(int index, int card)
     {
         uint playerTokenMask = 1u << index;
-        uint playersTokenBits = _playersTokenBits | playerTokenMask;
+        uint playersTokens = _playersTokens | playerTokenMask;
         uint cardAndSideToMove = unchecked((uint)card);
-        return new(playersTokenBits, cardAndSideToMove);
+        return new(playersTokens, cardAndSideToMove);
     }
 
     internal Node AddMinPlayerToken(int index, int card)
     {
         uint playerTokenMask = 1u << index;
-        uint playersTokenBits = _playersTokenBits | (playerTokenMask << Constants.CardCount);
-        uint cardAndSideToMove = unchecked((1u << 31) | (uint)card);
-        return new(playersTokenBits, cardAndSideToMove);
+        uint playersTokens = _playersTokens | (playerTokenMask << PlayerTokensBitCount);
+        uint cardAndSideToMove = unchecked((1u << CardBitCount) | (uint)card);
+        return new(playersTokens, cardAndSideToMove);
     }
 }
