8c01759056ab34a0630cc6c244fb51262b85a573
Viktor Ptitselov <qbit86@gmail.com>
2024-02-13 01:41:37 +0200

    Add project Samples.RollOut

---
+++ samples/Samples.RollOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using static System.FormattableString;
+
+namespace Okiya;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Random random = new(1729);
+        int[] cards = Enumerable.Range(0, Constants.CardCount).ToArray();
+        random.Shuffle(cards);
+        Solver solver = new(cards);
+        double evaluation = solver.Solve(out ImmutableStack<int> moves);
+        Console.WriteLine(Invariant($"{nameof(evaluation)}: {evaluation}"));
+        Console.WriteLine(Invariant($"{nameof(moves)}: {moves.Count()}"));
+    }
+}
