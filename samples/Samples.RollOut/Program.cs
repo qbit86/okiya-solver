d2fbf389ff59c96c978ee05f4bcf620570a50e86
Viktor Ptitselov <qbit86@gmail.com>
2024-02-13 02:31:29 +0200

    Add printing the moves

--- samples/Samples.RollOut/Program.cs
+++ samples/Samples.RollOut/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using static System.FormattableString;
 
@@ -13,8 +14,14 @@ internal static class Program
         int[] cards = Enumerable.Range(0, Constants.CardCount).ToArray();
         random.Shuffle(cards);
         Solver solver = new(cards);
+        var stopwatch = Stopwatch.StartNew();
         double evaluation = solver.Solve(out ImmutableStack<int> moves);
+        stopwatch.Stop();
+        Console.WriteLine($"Finished in {stopwatch.Elapsed}");
         Console.WriteLine(Invariant($"{nameof(evaluation)}: {evaluation}"));
-        Console.WriteLine(Invariant($"{nameof(moves)}: {moves.Count()}"));
+        Console.WriteLine(Invariant($"{nameof(moves)}.Count: {moves.Count()}"));
+        Console.WriteLine($"{nameof(moves)}:");
+        foreach (int move in moves)
+            Console.WriteLine($"\t{move} ({Int32CardConcept.Instance.ToString(cards[move])})");
     }
 }
