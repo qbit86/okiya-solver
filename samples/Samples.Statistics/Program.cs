e15ecd0fb278da45cdfd40ddaa84b79b9b150859
Viktor Ptitselov <qbit86@gmail.com>
2024-02-23 21:43:39 +0200

    Refine Statistics

--- samples/Samples.Statistics/Program.cs
+++ samples/Samples.Statistics/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.FormattableString;
 
 namespace Okiya;
 
@@ -13,25 +14,53 @@ internal static class Program
 {
     private static async Task Main()
     {
-        const int initialSeed = 141;
+        const int initialSeed = 2331;
         var stopwatch = new Stopwatch();
         List<SeedMoveScore> smsTuples = [];
         using CancellationTokenSource cts = new();
+        cts.CancelAfter(TimeSpan.FromMinutes(10.0));
         CancellationToken cancellationToken = cts.Token;
         int[] cards = GC.AllocateUninitializedArray<int>(Constants.CardCount);
         var countFuture = Task.Run(Enumerate, cancellationToken);
+#if false
         Task readLineFuture = Console.In.ReadLineAsync(cancellationToken).AsTask();
         await Task.WhenAny(countFuture, readLineFuture).ConfigureAwait(false);
         await cts.CancelAsync().ConfigureAwait(false);
+#endif
         int gameCount = await countFuture.ConfigureAwait(false);
         Console.WriteLine($"Finished in {stopwatch.Elapsed}");
+        Console.WriteLine($"{nameof(initialSeed)}: {initialSeed}");
         Console.WriteLine($"{nameof(gameCount)}: {gameCount}");
-        ILookup<int, SeedMoveScore> lookup = smsTuples.ToLookup(it => it.Score.CompareTo(0.0));
-        foreach (IGrouping<int, SeedMoveScore> grouping in lookup)
-            Console.WriteLine($"{grouping.Key}:\t{grouping.Count()}");
+        Console.WriteLine(Invariant($"Games per minute: {gameCount / stopwatch.Elapsed.TotalMinutes}"));
+        Console.WriteLine(Invariant($"Seconds per game: {stopwatch.Elapsed.TotalSeconds / gameCount}"));
+        ILookup<int, SeedMoveScore> smsTuplesByOutcome = smsTuples.ToLookup(it => it.Score.CompareTo(0.0));
+        Console.WriteLine("Outcomes:");
+        foreach (IGrouping<int, SeedMoveScore> grouping in smsTuplesByOutcome)
+        {
+            int groupingCount = grouping.Count();
+            decimal percentage = decimal.Divide(groupingCount, gameCount);
+            Console.WriteLine($"\t{grouping.Key}:\t{groupingCount} ({percentage:P})");
+        }
 
-        SeedMoveScore worstSeedScorePair = smsTuples.MinBy(it => it.Score);
-        Console.WriteLine($"{nameof(worstSeedScorePair)}: {worstSeedScorePair}");
+        SeedMoveScore worstResult = smsTuples.MinBy(it => it.Score);
+        Console.WriteLine($"{nameof(worstResult)}: {worstResult}");
+
+        ILookup<int, SeedMoveScore> smsTuplesByMove = smsTuplesByOutcome[1].ToLookup(it => it.Move);
+        IOrderedEnumerable<IGrouping<int, SeedMoveScore>> smsTuplesByMoveOrdered =
+            smsTuplesByMove.OrderBy(it => it.Key);
+        Console.WriteLine("Moves:");
+        foreach (IGrouping<int, SeedMoveScore> grouping in smsTuplesByMoveOrdered)
+            Console.WriteLine($"\t{grouping.Key}:\t{grouping.Count()}");
+
+        ILookup<bool, SeedMoveScore> smsTuplesByMoveClass = smsTuplesByOutcome[1].ToLookup(it => IsCorner(it.Move));
+        Console.WriteLine("Move classes:");
+        foreach (IGrouping<bool, SeedMoveScore> grouping in smsTuplesByMoveClass)
+        {
+            string moveClass = grouping.Key ? "Corner" : "Other";
+            int groupingCount = grouping.Count();
+            decimal percentage = decimal.Divide(groupingCount, smsTuplesByOutcome[1].Count());
+            Console.WriteLine($"\t{moveClass}:\t{groupingCount} ({percentage:P})");
+        }
 
         return;
 
@@ -45,8 +74,8 @@ internal static class Program
                 {
                     for (int i = 0; i < cards.Length; ++i)
                         cards[i] = i;
-                    Random random = new(seed);
-                    random.Shuffle(cards);
+                    Random prng = new(seed);
+                    prng.Shuffle(cards);
                     var game = Game.Create(cards);
                     Node rootNode = new();
                     var solver = Solver.Create(game, rootNode);
@@ -61,6 +90,11 @@ internal static class Program
             {
                 stopwatch.Stop();
             }
+        }
+
+        static bool IsCorner(int m)
+        {
+            return m is 0 or 3 or 12 or 15;
         }
     }
 }
