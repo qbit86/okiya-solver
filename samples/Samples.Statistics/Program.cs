63ff0cb82494ecfe6fdea27d6b252c4f2df4d999
Viktor Ptitselov <qbit86@gmail.com>
2024-02-23 02:17:52 +0200

    Implement proper awaiting

--- samples/Samples.Statistics/Program.cs
+++ samples/Samples.Statistics/Program.cs
@@ -14,16 +14,17 @@ internal static class Program
     private static async Task Main()
     {
         const int initialSeed = 1;
-        var stopwatch = Stopwatch.StartNew();
+        var stopwatch = new Stopwatch();
         List<SeedScorePair> seedScorePairs = [];
         using CancellationTokenSource cts = new();
         CancellationToken cancellationToken = cts.Token;
+        cts.CancelAfter(TimeSpan.FromMinutes(1.0));
         int[] cards = GC.AllocateUninitializedArray<int>(Constants.CardCount);
-        var countFuture = Task.Run(Enumerate, default);
-        Console.ReadLine();
+        var countFuture = Task.Run(Enumerate, cancellationToken);
+        Task readLineFuture = Console.In.ReadLineAsync(cancellationToken).AsTask();
+        await Task.WhenAny(countFuture, readLineFuture).ConfigureAwait(false);
         await cts.CancelAsync().ConfigureAwait(false);
         int gameCount = await countFuture.ConfigureAwait(false);
-        stopwatch.Stop();
         Console.WriteLine($"Finished in {stopwatch.Elapsed}");
         Console.WriteLine($"{nameof(gameCount)}: {gameCount}");
         ILookup<int, SeedScorePair> lookup = seedScorePairs.ToLookup(p => p.Score.CompareTo(0.0));
@@ -37,22 +38,30 @@ internal static class Program
 
         int Enumerate()
         {
-            int iterationCount = 0;
-            for (int seed = initialSeed; !cancellationToken.IsCancellationRequested; ++seed, ++iterationCount)
+            stopwatch.Restart();
+            try
             {
-                Random random = new(seed);
-                for (int i = 0; i < cards.Length; ++i)
-                    cards[i] = i;
-                random.Shuffle(cards);
-                var game = Game.Create(cards);
-                Node rootNode = new();
-                var solver = Solver.Create(game, rootNode);
-                if (!solver.TrySelectMove(out _, out double score))
-                    throw new InvalidOperationException();
-                seedScorePairs.Add((seed, score));
-            }
+                int iterationCount = 0;
+                for (int seed = initialSeed; !cancellationToken.IsCancellationRequested; ++seed, ++iterationCount)
+                {
+                    Random random = new(seed);
+                    for (int i = 0; i < cards.Length; ++i)
+                        cards[i] = i;
+                    random.Shuffle(cards);
+                    var game = Game.Create(cards);
+                    Node rootNode = new();
+                    var solver = Solver.Create(game, rootNode);
+                    if (!solver.TrySelectMove(out _, out double score))
+                        throw new InvalidOperationException();
+                    seedScorePairs.Add((seed, score));
+                }
 
-            return iterationCount;
+                return iterationCount;
+            }
+            finally
+            {
+                stopwatch.Stop();
+            }
         }
     }
 }
