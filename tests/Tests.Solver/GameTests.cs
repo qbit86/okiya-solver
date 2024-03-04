cb104c17fffd4db9daed0b734fe6c66ca998bdec
Viktor Ptitselov <qbit86@gmail.com>
2024-03-05 01:25:42 +0200

    Add TryMakeMove_WhenLegalMove_ReturnsFalse() testing method

--- tests/Tests.Solver/GameTests.cs
+++ tests/Tests.Solver/GameTests.cs
@@ -7,6 +7,8 @@ public sealed class GameTests
 {
     public static TheoryData<int[], Node, int> IllegalMoveTheoryData { get; } = CreateIllegalMoveTheoryData();
 
+    public static TheoryData<int[], Node, int, Node> LegalMoveTheoryData { get; } = CreateLegalMoveTheoryData();
+
     [Theory]
     [MemberData(nameof(IllegalMoveTheoryData), MemberType = typeof(GameTests))]
     public void TryMakeMove_WhenIllegalMove_ReturnsFalse(int[] cards, Node node, int move)
@@ -14,6 +16,16 @@ public sealed class GameTests
         var game = Game.Create(cards);
         bool actual = game.TryMakeMove(node, move, out _);
         Assert.False(actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(LegalMoveTheoryData), MemberType = typeof(GameTests))]
+    public void TryMakeMove_WhenLegalMove_ReturnsFalse(int[] cards, Node node, int move, Node expectedChild)
+    {
+        var game = Game.Create(cards);
+        bool actual = game.TryMakeMove(node, move, out Node actualChild);
+        Assert.True(actual);
+        Assert.Equal(expectedChild, actualChild);
     }
 
     private static TheoryData<int[], Node, int> CreateIllegalMoveTheoryData()
@@ -32,6 +44,23 @@ public sealed class GameTests
             { cards, new(), 5 },
             { cards, Node.Create(1, 0, 1, 0), 5 },
             { cards, Node.Create(1, 0, 1, 0), 0 }
+        };
+    }
+
+    private static TheoryData<int[], Node, int, Node> CreateLegalMoveTheoryData()
+    {
+        string[] cardStrings =
+        [
+            "K♥", "J♠", "J♥", "Q♣",
+            "Q♦", "J♣", "K♠", "A♠",
+            "J♦", "Q♠", "K♦", "Q♥",
+            "K♣", "A♦", "A♥", "A♣"
+        ];
+        int[] cards = cardStrings.Select(Int32CardConcept.Instance.Parse).ToArray();
+
+        return new()
+        {
+            { cards, new(), 1, Node.Create(0b10, 0, 1, 1) }
         };
     }
 }
